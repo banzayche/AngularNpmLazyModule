892d12c8ddbe7645d67189fc2cd95d1ebea25b06
banzayche <dharm.serg@gmail.com>
2019-02-10 21:04:41 +0200

    travis changes

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var dom = require('gulp-dom');
+const ghpPathPref = 'lazy-loaded-module-thru-npm/';
+
+function fixPathProcedure(arr, attrToFix, pref) {
+  for (let index = 0; index < arr.length; index++) {
+    const tag = arr[index];
+    let _attrToFix = `${pref}${tag[attrToFix]}`;
+    tag[attrToFix] = _attrToFix;
+  }
+}
+
+gulp.task('fix.ghp.paths', function () {
+  return gulp.src('./dist/lazy-loaded-module-thru-npm/index.html')
+    .pipe(dom(function () {
+      const scripts = {
+        nodes: this.querySelectorAll('script'),
+        attrToChange: 'src'
+      };
+      const links = {
+        nodes: this.querySelectorAll('link'),
+        attrToChange: 'href'
+      };
+
+      fixPathProcedure(scripts.nodes, scripts.attrToChange, ghpPathPref);
+      fixPathProcedure(links.nodes, links.attrToChange, ghpPathPref);
+
+      return this;
+    }))
+    .pipe(gulp.dest('./dist/lazy-loaded-module-thru-npm'));
+});
